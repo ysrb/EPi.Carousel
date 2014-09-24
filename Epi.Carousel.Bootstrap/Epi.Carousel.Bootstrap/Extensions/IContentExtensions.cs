820c2896a97754685f508c136deaf965ac1081b2
Regnārs Breikšs <rbreikss@gmail.com>
2014-09-24 14:45:35 +0300

    Initial version of Epi.Carousel.Bootstrap created.
    Initial version of Epi.Carousel.Bootstrap created.

---
+++ Epi.Carousel.Bootstrap/Epi.Carousel.Bootstrap/Extensions/IContentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.ServiceLocation;
+
+namespace Epi.Carousel.Bootstrap.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IContentExtensions
+    {
+        public static string GetCarouselSlidePartialViewName(this IContent content)
+        {
+            var contentTypeRepository = ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+            var contentType = contentTypeRepository.Load(content.ContentTypeID);
+
+            return string.Format("~/Views/Shared/Blocks/Carousel{0}.cshtml", contentType.Name);
+        }
+    }
+
+}
---
+++ Epi.Carousel.Bootstrap/Epi.Carousel.Bootstrap/Models/BlockTypes/Carousel/SlideLinkBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace Epi.Carousel.Bootstrap.Models.BlockTypes.Carousel
+{
+    [ContentType(AvailableInEditMode = false, GUID = "46FB2297-2CEC-45E6-BC9C-6F59305C53EC")]
+    public class SlideLinkBlock : BlockData
+    {
+        [Display(Order = 100)]
+        public virtual string Text { get; set; }
+
+        [Display(Order = 110)]
+        public virtual Url Link { get; set; }
+
+        public bool HasValue()
+        {
+            return Link != null
+                && !Link.IsEmpty()
+                && !string.IsNullOrWhiteSpace(Text);
+        }
+    }
+}

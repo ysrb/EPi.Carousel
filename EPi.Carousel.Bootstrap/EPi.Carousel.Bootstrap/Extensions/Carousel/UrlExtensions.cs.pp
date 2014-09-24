f5c66e0c6b07c021b74e32c0890b098a1b0f9120
Regnārs Breikšs <rbreikss@gmail.com>
2014-09-24 17:29:12 +0300

    EPi.Carousel.Bootstrap re-added to repository.

---
+++ EPi.Carousel.Bootstrap/EPi.Carousel.Bootstrap/Extensions/Carousel/UrlExtensions.cs.pp
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using EPiServer;
+using Geta.EPi.Extensions.QueryString;
+
+namespace $rootnamespace$.Extensions.Carousel
+{
+    public static class UrlExtensions
+    {
+        public static string GetResizedImageUrl(this Url url, int width, int height)
+        {
+            return QueryStringBuilder.Create(url.ToString())
+                .Add("w", width.ToString(CultureInfo.InvariantCulture))
+                .Add("h", height.ToString(CultureInfo.InvariantCulture))
+                .ToHtmlString();
+        }
+    }
+}
---
+++ EPi.Carousel.Bootstrap/EPi.Carousel.Bootstrap/Views/Shared/Blocks/CarouselSlideBlock.cshtml.pp
@@ -0,0 +1,20 @@
+﻿@using $rootnamespace$.Extensions.Carousel
+@using EPiServer.Web.Mvc.Html
+@using $rootnamespace$.Models.BlockTypes.Carousel
+@model SlideBlock
+
+<img class="img-responsive" src="@Model.ImageUrl.GetResizedImageUrl((int)ViewBag.ImageWidth, (int)ViewBag.ImageHeight)" alt="@Model.ImageUrl">
+<div class="carousel-caption">
+    <h2 class="animated fadeInLeftBig">
+        @Html.PropertyFor(m => m.Title)
+    </h2>
+    <div class="animated fadeInRightBig">@Html.PropertyFor(m => m.Description)</div>
+    @if(Model.ButtonLink.HasValue())
+    {
+        <a class="animated fadeInLeftBig btn btn-info btn-lg" href="@Url.ContentUrl(Model.ButtonLink.Link)" @Html.EditAttributes(m => m.ButtonLink)>
+            @Model.ButtonLink.Text
+        </a>
+    }
+</div>
+
+@Html.FullRefreshPropertiesMetaData(new[] { "ButtonLink" })
